1edcfe34153e8430e5397ac9dbf1666cd271dd71
ke-sano <sanok.jp@protonmail.com>
2024-03-07 23:35:05 +0900

    fix

--- app/build/libjxl-wasm.sh
+++ app/build/libjxl-wasm.sh
@@ -14,7 +14,7 @@ git clone -b v0.10.1 https://github.com/libjxl/libjxl.git --recursive --shallow-
 
 cd libjxl
 
-BUILD_TARGET=wasm32 ENABLE_WASM_SIMD=1 EMCC_CFLAGS="" emconfigure ./ci.sh release
+BUILD_TARGET=wasm32 ENABLE_WASM_SIMD=1 EMCC_CFLAGS="-O3 -flto" emconfigure ./ci.sh release
 
 apt-get purge --auto-remove -y \
 git \
